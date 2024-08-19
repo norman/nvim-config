0bf5e8f44fc489bfcb4d6b28371307707d66ee7d
Norman Clarke <norman@njclarke.com>
2024-08-19 14:30:14 -0300

    More setup

--- init.lua
+++ init.lua
@@ -1,41 +1,49 @@
+---------------------------------------------------------------
+---------------------------------------------------------------
+--- This part of the config should all just be vanilla Lua. ---
+---------------------------------------------------------------
+---------------------------------------------------------------
+
+local colorscheme = "evening"
+
 local global_options = {
   -- Use comma as the leader for keybindings
   mapleader = ","
 }
 
 local options = {
-  -- Location of ctags file. Used for basic class/function navigation and
-  -- opening files by class or function name with `vim -t`.
-  tags = "./tags",
+  -- Insert the configured number of spaces instead of a tab
+  expandtab = true,
 
   -- Use line numbers
   number = true,
+
+  -- The number of space characters inserted for indentation
+  shiftwidth = 2,
+
+  -- Indent automatically when starting a new line
+  smartindent = true,
+
+  -- Basically make spaces behave like tabs for code indentation
+  smarttab = true,
 
   -- How many spaces does a tab represent? This should be somewhat
   -- language-dependent but I primarily use Ruby so my default is 2.
   tabstop = 2,
 
-  -- The number of space characters inserted for indentation
-  shiftwidth = 2,
-
-  -- Basically make spaces behave like tabs for code indentation
-  smarttab = true,
-
-  -- Indent automatically when starting a new line
-  smartindent = true,
-
-  -- Insert the configured number of spaces instead of a tab
-  expandtab = true
+  -- Location of ctags file. Used for basic class/function navigation and
+  -- opening files by class or function name with `vim -t`.
+  tags = "./tags",
 }
 
 local keymaps_with_leader = {
-  {keys = "s",  cmd = "set nolist!",   desc = "Toggle invisible characters"},
-  {keys = "n",  cmd = "set nonumber!", desc = "Toggle line numbers"},
-  {keys = "f",  cmd = "Telescope find_files", desc = "Telescope find files"},
-  {keys = "t",  cmd = "Telescope tags", desc = "Telescope tags"},
-  {keys = "b",  cmd = "Telescope buffers", desc = "Telescope buffers"},
-  {keys = "gf", cmd = "Telescope git_files", desc = "Telescope Git files"},
-  {keys = "lg", cmd = "Telescope live_grep", desc = "Telescope live grep"},
+  {keys = "b",  cmd = "Telescope buffers",        desc = "Telescope buffers"},
+  {keys = "f",  cmd = "Telescope find_files",     desc = "Telescope find files"},
+  {keys = "n",  cmd = "set nonumber!",            desc = "Toggle line numbers"},
+  {keys = "s",  cmd = "set nolist!",              desc = "Toggle invisible characters"},
+  {keys = "t",  cmd = "Telescope tags",           desc = "Telescope tags"},
+  {keys = "gf", cmd = "Telescope git_files",      desc = "Telescope Git files"},
+  {keys = "lg", cmd = "Telescope live_grep",      desc = "Telescope live grep"},
   {keys = "lr", cmd = "Telescope lsp_references", desc = "Telescope LSP References"}
 }
 
@@ -44,6 +52,7 @@ local keymaps_with_leader = {
 ---- No more configuration after this comment, just setup. ----
 ---------------------------------------------------------------
 ---------------------------------------------------------------
+vim.cmd(string.format("colorscheme %s", colorscheme))
 
 -- Assign global configuration
 for name, value in pairs(global_options) do
