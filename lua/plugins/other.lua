de6ca0c97a197191aa80a29e7ebb34044090d71d
Norman Clarke <norman@njclarke.com>
2024-08-22 12:35:38 -0300

    Set up test for "Other" mappings

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -3,65 +3,7 @@ return {
   config = function()
     require("other-nvim").setup({
       showMissingFiles = false,
-      mappings = {
-        {
-          pattern = "/lib/tasks/([^/]*).rake",
-          target = "/spec/lib/tasks/%1_spec.rb"
-        },
-        {
-          pattern = "/spec/lib/tasks/([^/]*)_spec.rb",
-          target = "/lib/tasks/%1.rake"
-        },
-        {
-          pattern = "/lib/([^/]*).rb",
-          target = "/spec/lib/%1_spec.rb"
-        },
-        {
-          pattern = "/spec/lib/([^/]*)_spec.rb",
-          target = "/lib/%1.rb"
-        },
-        {
-          pattern = "/app/([^/]*)/([^/]*).rb",
-          target = "/spec/%1/%2_spec.rb"
-        },
-        {
-          pattern = "/spec/([^/]*)/([^/]*)_spec.rb",
-          target = "/app/%1/%2.rb"
-        },
-        {
-          pattern = "/app/([^/]*)/([^/]*)/([^/]*).rb",
-          target = "/spec/%1/%2/%3_spec.rb"
-        },
-        {
-          pattern = "/spec/([^/]*)/([^/]*)/([^/]*)_spec.rb",
-          target = {
-            {
-              target = "/app/%1/%2/%3.rb",
-              context = "app"
-            },
-            {
-              target = "/%1/%2/%3.rake",
-              context = "tasks"
-            }
-          }
-        },
-        {
-          pattern = "/app/([^/]*)/([^/]*)/([^/]*)/([^/]*).rb",
-          target = "/spec/%1/%2/%3/%4_spec.rb"
-        },
-        {
-          pattern = "/spec/([^/]*)/([^/]*)/([^/]*)/([^/]*)_spec.rb",
-          target = "/app/%1/%2/%3/%4.rb"
-        },
-      {
-        pattern = "/app/views/(.*)/(.*)",
-        target = "/spec/views/%1/%2_spec.rb",
-      },
-      {
-        pattern = "/spec/views/(.*)/(.*)_spec.rb",
-        target = "/app/views/%1/%2",
-      },
-      }
+      mappings = dofile(os.getenv("HOME") .. "/.config/nvim/lua/other_mappings.lua")
     })
   end
 }
