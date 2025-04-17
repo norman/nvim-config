58e16e5e63de5a28c72e93377050c8c51ff988d9
Norman Clarke <norman@njclarke.com>
2025-04-17 19:16:03 -0300

    Added mostly stock Ruby-LSP config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,11 +9,47 @@ return {
     -- }
 
     lspconfig.ruby_lsp.setup({
-      cmd = { os.getenv("HOME") .. "/.rbenv/shims/ruby-lsp" },
+      cmd = { "bundle", "exec", "ruby-lsp" },
+      diagnostics = true,
+      filetypes = { "ruby", "eruby" },
+      -- root_markers = { 'Gemfile', '.git' },
       init_options = {
-        formatter = "rubocop_internal",
+        formatter = "auto",
         linters = { "rubocop" },
-      },
+        enabledFeatures = {
+          codeActions = true,
+          codeLens = true,
+          completion = true,
+          definition = true,
+          diagnostics = true,
+          documentHighlights = true,
+          documentLink = true,
+          documentSymbols = true,
+          foldingRanges = true,
+          formatting = true,
+          hover = true,
+          inlayHint = true,
+          onTypeFormatting = false,
+          selectionRanges = true,
+          semanticHighlighting = true,
+          signatureHelp = true,
+          typeHierarchy = true,
+          workspaceSymbol = true
+        },
+        featuresConfiguration = {
+          inlayHint = {
+            implicitHashValue = true,
+            implicitRescue = true
+          }
+        },
+        indexing = {
+          excludedPatterns = { "path/to/excluded/file.rb" },
+          includedPatterns = { "path/to/included/file.rb" },
+          excludedGems = {},
+          excludedMagicComments = {}
+        },
+        experimentalFeaturesEnabled = false
+      }
     })
 
     lspconfig.eslint.setup {}
@@ -23,11 +59,11 @@ return {
     lspconfig.lua_ls.setup {
       on_init = function(client)
         local path = client.workspace_folders[1].name
-        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
           return
         end
 
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
           runtime = {
             -- Tell the language server which version of Lua you're using
             -- (most likely LuaJIT in the case of Neovim)
@@ -55,9 +91,9 @@ return {
               indent_style = "space",
               indent_size = "2",
               quote_style = "double",
-              trailing_table_separator = "never",
+              trailing_table_separator = "never"
             }
-          },
+          }
         }
       }
     }
